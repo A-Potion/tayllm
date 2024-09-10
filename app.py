506b31361fc2eca896c4a1e274b3e60492dc7844
Kacper <07Potion@gmail.com>
2024-09-09 20:23:39 -0400

    Align app defs

--- app.py
+++ app.py
@@ -33,6 +33,27 @@ encode = lambda s: [stoi[c] for c in s] # Take a string, return a list of intege
 decode = lambda l: ''.join([itos[i] for i in l]) # Take a list of integers, return a string
 
 
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 class Head(nn.Module):
 
     def __init__(self, head_size):
